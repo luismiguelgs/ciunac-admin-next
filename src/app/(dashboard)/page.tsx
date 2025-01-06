91a6cc5752be103a10e4e5e8e2b80e6f29698f9d
Luis Miguel <luismiguel.gs@outlook.com>
2025-01-06 14:32:06 -0500

    Fix Dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,7 +5,7 @@ import SolicitudesService from '@/services/solicitudes.service';
 //import MyLineChart from './(components)/MyLineChart';
 import { auth } from '@/auth';
 import { Typography } from '@mui/material';
-import MyBarChartH from './(components)/MyBarChartH';
+//import MyBarChartH from './(components)/MyBarChartH';
 //import MyBarChartV from './(components)/MyBarChartV';
 
 async function getData() {
@@ -29,12 +29,13 @@ export default async function Page()
                     <MyPieChart data={data}/>
                 </CardChart>
             </Grid>
+            {/*
             <Grid size={{xs:12, sm:6}}>
                 <CardChart title='Solicitudes por facultad'>
                     <MyBarChartH data={data} />
                 </CardChart>
             </Grid>
-            {/*
+            
             <Grid size={{xs:12, sm:6}}>
                 <CardChart title='Solicitudes por mes'>
                     <MyLineChart data={data}/>
